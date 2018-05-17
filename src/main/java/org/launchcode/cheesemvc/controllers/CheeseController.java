7f56afbfdbfe109a1957d84232273037ba37e90f
sumit <sumitsenpandey@gmail.com>
2018-05-16 20:26:30 -0500

    Initial Commit

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
-    static ArrayList<String> cheeses = new ArrayList<>();
+    static HashMap<String,String> cheeses = new HashMap<>();
 
 
     @RequestMapping(value="")
@@ -33,8 +34,9 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add",method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName){
-        cheeses.add(cheeseName);
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription){
+
+        cheeses.put(cheeseName,cheeseDescription);
         //redirect to /cheese
         return "redirect:";
     }
