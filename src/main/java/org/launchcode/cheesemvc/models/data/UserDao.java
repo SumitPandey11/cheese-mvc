13b9211bf100736d0145a98472215fe75bf8c5d5
sumit <sumitsenpandey@gmail.com>
2018-05-28 17:44:54 -0500

    Implemented Persisting Objects with JPA

--- src/main/java/org/launchcode/cheesemvc/models/data/UserDao.java
+++ src/main/java/org/launchcode/cheesemvc/models/data/UserDao.java
@@ -6,6 +6,11 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
+/*
+
+@Transactional - Most often used on CrudRepository interfaces. Specifies that if any data operation within the class throws a runtime exception,
+all associated database queries should be rolled back (that is, undone).  This can prevent data corruption caused by exceptions.
+ */
 @Repository
 @Transactional
 public interface UserDao extends CrudRepository<User, Integer> {
